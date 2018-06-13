112453e47405a44fd503191f95829008c3795eed
Aetaris <jeremiah.starborn@gmail.com>
2018-06-13 10:25:52 -0700

    Added basic vector class

---
+++ src/vector.js
@@ -0,0 +1,21 @@
+// vector class
+
+class Vector { // eslint-disable-line no-unused-vars
+  constructor (x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  // INSTANCE functions
+
+  distance (other) { // calculates the distance to another vector
+    return Math.sqrt(Math.pow(other.x - this.x, 2) + Math.pow(other.y - this.y, 2));
+    // looks fancy but it's just the pythagorean theorem
+  }
+
+  // STATIC functions
+
+  static distance (vec1, vec2) { // calculates distance between two vectors
+    return vec1.distance(vec2);
+  }
+}
