3f4d919bfa1da7e54c5f965600d5ee89c7e66fd1
Aetaris <jeremiah.starborn@gmail.com>
2018-06-12 19:26:23 -0700

    added player (again??)

--- src/entities.js
+++ src/entities.js
@@ -5,3 +5,13 @@ class Creature { // eslint-disable-line no-unused-vars
     this.name = 'Nameless';
   }
 }
+
+class Player extends Creature {
+  constructor () {
+    super();
+    this.name = 'Player';
+  }
+}
+
+var creat = new Player();
+console.log(creat.name); // literally just bc otherwise Creature is marked as 'unused'
