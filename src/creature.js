9a15f597f13951664b6be2e0480659d427ba4c5b
Aetaris <jeremiah.starborn@gmail.com>
2018-06-12 19:25:02 -0700

    added player class

--- src/creature.js
+++ src/creature.js
@@ -6,5 +6,12 @@ class Creature {
   }
 }
 
-var creat = new Creature();
+class Player extends Creature {
+  constructor () {
+    super();
+    this.name = 'Player';
+  }
+}
+
+var creat = new Player();
 console.log(creat.name); // literally just bc otherwise Creature is marked as 'unused'
