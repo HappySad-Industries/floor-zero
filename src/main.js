de2b1dae497df2ddf006aa9930d8e6214f33919f
Baelyk <b43lyk@gmail.com>
2018-06-12 18:40:40 -0700

    Create pattern

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
+/* globals Image */
 
-console.log("hello world")
+console.log('hello world');
 
-var canvas;
+let canvas, context;
 
 function initialize () {
-  canvas = document.getElementById("canvas");
+  let tile = new Image();
+  tile.src = 'assets/tile.png';
+
+  canvas = document.getElementById('canvas');
+  context = canvas.getContext('2d');
+
   canvas.width = 800;
   canvas.height = 500;
-  canvas.style.backgroundColor = "black";
+  canvas.style.backgroundColor = 'black';
+
+  tile.onload = event => {
+    let pattern = context.createPattern(tile, 'repeat');
+    context.fillStyle = pattern;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  };
 }
 
 initialize();
