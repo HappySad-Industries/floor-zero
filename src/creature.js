7ef43479cde029ab018847b346d231d15afe6a72
Aetaris <jeremiah.starborn@gmail.com>
2018-06-12 19:19:02 -0700

    added creature class

---
+++ src/creature.js
@@ -0,0 +1,10 @@
+// the creature class
+
+class Creature {
+  constructor () {
+    this.name = 'Nameless';
+  }
+}
+
+var creat = new Creature();
+console.log(creat.name); // literally just bc otherwise Creature is marked as 'unused'
