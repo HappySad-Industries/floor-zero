694a43225abfced4dc92086b7d08205be25913a6
Aetaris <jeremiah.starborn@gmail.com>
2018-06-12 18:38:30 -0700

    centered canvas

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 
-console.log("hello world")
+console.log('hello world');
 
 var canvas;
 
 function initialize () {
-  canvas = document.getElementById("canvas");
+  canvas = document.getElementById('canvas');
   canvas.width = 800;
   canvas.height = 500;
-  canvas.style.backgroundColor = "black";
-  canvas.style.textAlign = "center";
+  canvas.style.backgroundColor = 'black';
+  var container = document.getElementById('container');
+  container.style.textAlign = 'center';
 }
 
 initialize();
